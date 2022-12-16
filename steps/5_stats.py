7743aa9e56c4d7917899d9d978be09964f661fdc
beiming <beiming.cao@utexas.edu>
2022-12-15 22:35:17 -0600

    articulation rate

--- steps/5_stats.py
+++ steps/5_stats.py
@@ -6,6 +6,7 @@ from transformers import Wav2Vec2Processor, Wav2Vec2ForCTC
 from datasets import load_dataset
 import torch
 import librosa
+import textgrid
 
 
 def stats(args):
@@ -18,7 +19,9 @@ def stats(args):
     
     task_list = os.listdir(input_folder)
     total_t, total_wrd_num = 0.0, 0
-    task_sr = {}
+    total_t_ph, total_phn_num = 0.0, 0
+    task_sr = {} # speaking rate
+    task_ar = {} # articulation rate
     for task in task_list:
         task_spk_wrd = os.path.join(input_folder, task, speaker)
         task_spk_textgrid = task_spk_wrd + '_PHONE'
@@ -44,13 +47,38 @@ def stats(args):
         task_sr[task] = task_speaking_rate
         total_t += t
         total_wrd_num += wrd_num
+        
+        ### Articulation rate ###
+        txtgrid_path_list = glob.glob(task_spk_textgrid + '/*.TextGrid')
+        task_phone_time = 0.0
+        task_phone_num = 0
+        for i in range(len(txtgrid_path_list)):            
+
+            tg = textgrid.TextGrid.fromFile(txtgrid_path_list[i])
+            ph_tg = tg[1]            
+            for j in range(len(ph_tg)):
+                if ph_tg[j].mark != None and ph_tg[j].mark != 'spn':
+                    task_phone_num += 1 
+                    _pt = ph_tg[j].maxTime - ph_tg[j].minTime               
+                    task_phone_time += _pt
+                    
+        task_ar[task] = task_phone_num / task_phone_time
+        total_t_ph += task_phone_time
+        total_phn_num += task_phone_num
     total_speaking_rate = total_wrd_num / total_t
+    total_articulation_rate = total_phn_num / total_t_ph
     output_file = os.path.join(out_path, 'stats.txt')
     f = open(output_file, 'w')
     for task_name, sr in task_sr.items():
-        s = 'The speaker rate of task: ' + task_name + ' is ' + str(sr) + '\n'
+        s = 'The speaking rate of task: ' + task_name + ' is ' + str(sr) + '\n'
         f.write(s)
-    ss = 'The overall speaker rate is ' + str(total_speaking_rate) + '\n'
+    ss = 'The overall speaking rate is ' + str(total_speaking_rate) + '\n'
+    f.write(ss)
+    
+    for task_name, ar in task_ar.items():
+        s = 'The articulation rate of task: ' + task_name + ' is ' + str(ar) + '\n'
+        f.write(s)
+    ss = 'The overall articulation rate is ' + str(total_articulation_rate) + '\n'
     f.write(ss)
 
     f.close()
